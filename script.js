f336ef39d14ad4c18b64cf20c034bbaa61e125fd
biroveyou <biroveyou@gmail.com>
2026-06-22 16:52:47 +0000

    Fix createGrid function and add mouse events

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 "use strict";
 
 function createGrid(number) {
-    const container = document.querySelector(".container")
-    for (let i = 0; i < (number * number); i++) {
-        const div = document.createElement("div");
-        div.classList.add("square-div");
-        container.appendChild(div);
+    for (let i = 0; i < number; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row-div");
+        for (let j = 0; j < number; j++) {
+            const div = document.createElement("div");
+            div.classList.add("square-div");
+            row.appendChild(div);
+        }
+        container.appendChild(row);
     }
 }
 
-createGrid(4);
+const container = document.querySelector(".container");
+createGrid(16);
+
+const rows = document.querySelectorAll(".row-div");
+console.log(rows);
+for (let row of rows) {
+    row.addEventListener("mouseover", (e) => {
+        let target = event.target;
+        if (target && container.contains(target) && !(target == row)) {
+            console.log(target);
+            target.classList.add("inked");
+        }
+    });
+}
+
+
